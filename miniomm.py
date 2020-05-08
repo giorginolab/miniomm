605bca26fa7842d45b9d0db9133d0c6c46402121
Toni <toni.giorgino@gmail.com>
2020-05-08 13:29:28 +0200

    initial

--- miniomm.py
+++ miniomm.py
@@ -1,36 +1,33 @@
 
 
 import sys
-from sys import stdout
+import os.path
 
 from simtk.openmm import app
 import simtk.openmm as mm
 from simtk import unit
 
 
-def main():
+def main(runtype="NVT"):
 
-    prmtop_file="system.prmtop"
-    coord_file="input.coor"
+    system_basename = "structure"
 
-    out = "output"
-    output_file=f"{out}.dcd"
-    log_file=f"{out}.log"
-    checkpoint_file=f"{out}.chk"
-    
+    output_basename = "output"
+
     dt = 5.0 * unit.femtosecond
     T = 300 * unit.kelvin
     energyFreq = 1 * unit.picosecond
     trajectoryFreq = 100 * unit.picosecond
-    restartFreq = trajectoryFreq
+    # restartFreq = trajectoryFreq
     equilibrationTime = 100 * unit.picosecond
     runTime = 1 * unit.nanosecond
 
+    checkpoint_file = "restart.chk"
     nonbondedCutoff = 1.0*unit.nanometers
     frictionCoefficient = 1 / unit.picosecond
 
-    pdb = app.PDBFile('input.pdb')
-    #forcefield = app.ForceField('amber99sbildn.xml', 'tip3p.xml')
+    pdb = app.PDBFile(f'{system_basename}.pdb')
+    # forcefield = app.ForceField('amber99sbildn.xml', 'tip3p.xml')
 
     """
     system = forcefield.createSystem(pdb.topology,
@@ -41,11 +38,22 @@ def main():
                                      ewaldErrorTolerance=0.0005)
     """
 
-    prmtop = AmberPrmtopFile(prmtop_file)
-    system = prmtop.createSystem(nonbondedMethod=app.PME,
-                                 nonbondedCutoff=nonbondedCutoff,
-                                 constraints=app.AllBonds)
-    
+    if os.path.exists(f"{system_basename}.prmtop"):
+        prmtop = app.AmberPrmtopFile(f"{system_basename}.prmtop")
+        system = prmtop.createSystem(nonbondedMethod=app.PME,
+                                     nonbondedCutoff=nonbondedCutoff,
+                                     constraints=app.AllBonds)
+        topology = prmtop.topology
+    elif os.path.exists(f"{system_basename}.psf"):
+        psf = app.CharmmPsfFile('{system_basename}.psf')
+        params = app.CharmmParameterSet("parameters")
+        system = psf.createSystem(params,
+                                  nonbondedMethod=app.PME,
+                                  nonbondedCutoff=nonbondedCutoff,
+                                  constraints=app.AllBonds)
+    else:
+        raise FileNotFoundError("No AMBER nor CHARMM run files found")
+
     integrator = mm.LangevinIntegrator(T,
                                        frictionCoefficient,
                                        dt)
@@ -56,7 +64,7 @@ def main():
     properties = {'CudaPrecision': 'mixed'}
     """
 
-    simulation = app.Simulation(pdb.topology, system, integrator)
+    simulation = app.Simulation(topology, system, integrator)
     # ... , platform, properties
 
     try:
@@ -74,12 +82,13 @@ def main():
     print('Equilibrating...')
     simulation.step(equilibrationTime/dt)
 
-    simulation.reporters.append(app.DCDReporter(output_file,
+    simulation.reporters.append(app.DCDReporter(f"{output_basename}.dcd",
                                                 energyFreq/dt,
                                                 append=True))
     simulation.reporters.append(app.CheckPointReporter(checkpoint_file,
-                                                       trajectoryFreq/dt)
-    simulation.reporters.append(app.StateDataReporter(stdout, trajectoryFreq/dt,
+                                                       trajectoryFreq/dt))
+    simulation.reporters.append(app.StateDataReporter(sys.stdout,
+                                                      trajectoryFreq/dt,
                                                       step=True,
                                                       time=True,
                                                       potentialEnergy=True,
@@ -92,7 +101,8 @@ def main():
                                                       speed=True,
                                                       totalSteps=runTime/dt,
                                                       separator='\t'))
-    simulation.reporters.append(app.StateDataReporter(log_file, trajectoryFreq/dt,
+    simulation.reporters.append(app.StateDataReporter(f"{output_basename}.log",
+                                                      trajectoryFreq/dt,
                                                       step=True,
                                                       time=True,
                                                       potentialEnergy=True,
@@ -110,10 +120,8 @@ def main():
     simulation.step(runTime/dt)
 
     print('Done!')
+    return
 
 
-
- if __name__ == "__main__":
-     main()
-
-
+if __name__ == "__main__":
+    main()
